357310d27153c6b975c7688d2066fa5c08d81236
Muthamaki Creations <muthamakicreations@gmail.com>
2024-02-15 18:56:31 +0000

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
 import sys
 
-def is_safe(board, row, col, N):
+def is_safe(board, row, col):
     # Check if there is a queen in the same column
     for i in range(row):
-        if board[i][col] == 'Q':
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
             return False
-
-    # Check upper left diagonal
-    for i, j in zip(range(row-1, -1, -1), range(col-1, -1, -1)):
-        if board[i][j] == 'Q':
-            return False
-
-    # Check upper right diagonal
-    for i, j in zip(range(row-1, -1, -1), range(col+1, N)):
-        if board[i][j] == 'Q':
-            return False
-
     return True
 
 def solve_n_queens_util(board, row, N, solutions):
     if row == N:
-        solutions.append([''.join(row) for row in board])
+        solutions.append([board[i] for i in range(N)])
         return
 
     for col in range(N):
-        if is_safe(board, row, col, N):
-            board[row][col] = 'Q'
+        if is_safe(board, row, col):
+            board[row] = col
             solve_n_queens_util(board, row+1, N, solutions)
-            board[row][col] = '.'
 
 def solve_n_queens(N):
     if not N.isdigit():
@@ -40,13 +30,12 @@ def solve_n_queens(N):
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [['.' for _ in range(N)] for _ in range(N)]
+    board = [-1] * N
     solutions = []
     solve_n_queens_util(board, 0, N, solutions)
 
     for solution in solutions:
-        print("\n".join(solution))
-        print()
+        print([[i, solution[i]] for i in range(N)])
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
