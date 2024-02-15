9dc66e3ff1bc28433ca9d0f9c22a27b036885054
Muthamaki Creations <muthamakicreations@gmail.com>
2024-02-15 19:13:52 +0000

    Update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,60 +4,30 @@ const request = require('request');
 
 const movieId = process.argv[2];
 
-if (!movieId || isNaN(movieId)) {
-  console.error('Please provide a valid movie ID as the first argument.');
-  process.exit(1);
-}
-
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Unexpected status code:', response.statusCode);
-    process.exit(1);
-  }
-
-  const filmData = JSON.parse(body);
-  const characters = filmData.characters;
-
-  if (!characters || characters.length === 0) {
-    console.error('No characters found for this movie.');
-    process.exit(1);
-  }
-
-  // Fetch character names
-  const fetchCharacterNames = async () => {
-    try {
-      for (const characterUrl of characters) {
-        const characterData = await fetchCharacterData(characterUrl);
-        console.log(characterData.name);
-      }
-    } catch (error) {
-      console.error('Error fetching character data:', error);
-    }
-  };
-
-  const fetchCharacterData = (url) => {
-    return new Promise((resolve, reject) => {
-      request(url, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          if (response.statusCode === 200) {
-            resolve(JSON.parse(body));
+  } else {
+    const film = JSON.parse(body);
+    const charactersUrls = film.characters;
+    const promises = charactersUrls.map(url =>
+      new Promise((resolve, reject) => {
+        request(url, (error, response, body) => {
+          if (error) {
+            reject(error);
           } else {
-            reject(`Unexpected status code: ${response.statusCode}`);
+            const character = JSON.parse(body);
+            resolve(character.name);
           }
-        }
-      });
-    });
-  };
-
-  // Start fetching character names
-  fetchCharacterNames();
+        });
+      })
+    );
+    Promise.all(promises)
+      .then(characters => {
+        characters.forEach(character => console.log(character));
+      })
+      .catch(error => console.error('Error:', error));
+  }
 });
