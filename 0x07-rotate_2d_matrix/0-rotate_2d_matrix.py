77637d6b99fd997f151fef59ba27df06aff77cf8
Muthamaki Creations <muthamakicreations@gmail.com>
2024-03-25 17:15:12 +0000

    commit 1

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,14 +1,35 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
+"""
+Rotate 2D Matrix
 """
 
 def rotate_2d_matrix(matrix):
+    """
+    Rotates a 2D matrix 90 degrees clockwise in-place.
+    Args:
+        matrix (List[List[int]]): The 2D matrix to rotate.
+    """
     n = len(matrix)
-    # Transpose the matrix
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    # Reverse each row
-    for row in matrix:
-        row.reverse()
+    # Traverse each layer of the matrix
+    for layer in range(n // 2):
+        # Iterate over each element in the layer
+        for i in range(layer, n - layer - 1):
+            # Save top element
+            temp = matrix[layer][i]
+            # Move left to top
+            matrix[layer][i] = matrix[n - i - 1][layer]
+            # Move bottom to left
+            matrix[n - i - 1][layer] = matrix[n - layer - 1][n - i - 1]
+            # Move right to bottom
+            matrix[n - layer - 1][n - i - 1] = matrix[i][n - layer - 1]
+            # Move top to right
+            matrix[i][n - layer - 1] = temp
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+
+    rotate_2d_matrix(matrix)
+    print(matrix)
